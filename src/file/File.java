54c10832ff23b8189c40ba6349ba919382ea3300
Lucas-Garavaglia <48389445+Lucas-Garavaglia@users.noreply.github.com>
2019-10-31 13:54:56 -0300

    Update File.java

--- src/file/File.java
+++ src/file/File.java
@@ -1,26 +1,37 @@
 package file;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 //Class that manipulates a file
 public class File {
-    String fileName;
-
-    //File class constructor
-    public File(String fileName){
-        this.fileName = fileName;
+        
+    public Boolean WordChecks(String Line) {
+    	return Line.matches("[A-Za-z]+");
     }
-
-    //Overlodade File class constructor
-    public File(){
-        this.fileName = "NULL";
+    
+    public void loadFileInsert(String WayFile) throws Exception{
+        FileReader Read = new FileReader(WayFile);
+        BufferedReader ReadFile = new BufferedReader(Read);
+        String Line = ReadFile.readLine();
+        while(Line != null) {
+        	if(this.WordChecks(Line)) {
+        		Line.toLowerCase();
+//				inserir();
+        	}
+        	Line = ReadFile.readLine();        	
+        }
+        ReadFile.close();
     }
-
-    //fileName setter
-    public void setFileName(String fileName){
-        this.fileName = fileName;
-    }
-
-    //fileName getter
-    public String getFileName(){
-        return this.fileName;
+    public void loadFileRemove(String WayFile) throws Exception{
+        FileReader Read = new FileReader(WayFile);
+        BufferedReader ReadFile = new BufferedReader(Read);
+        String Line = ReadFile.readLine();
+        while(Line != null) {
+        	Line.toLowerCase();
+//			remove();
+        	Line = ReadFile.readLine();        	
+        }
+        ReadFile.close();
     }
 }
