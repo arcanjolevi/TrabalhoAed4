ea0cf191ab8b41ced0c034aeb15fe8e0a8829cb0
CaioslppUO <37354152+CaioslppUO@users.noreply.github.com>
2019-11-06 20:22:26 -0300

    Delete Trie.java

--- src/Structure/Trie.java
+++
@@ -1,9 +0,0 @@
-package Structure;
-
-import Structure.Node;
-
-//Class that represents a Trie tree
-public class Trie{
-    private Node root;      //Root from Trie tree
-    private String word;    //Currently formed word
-}
