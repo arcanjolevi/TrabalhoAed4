7c353cab356ea890cda47f7db9678eb46f99fe10
CaioslppUO <37354152+CaioslppUO@users.noreply.github.com>
2019-11-23 12:13:32 -0200

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,10 @@ package main;
 
 import structure.*;
 import window.Window;
-import file.File;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        File f = new File();
         TrieRoot t = new TrieRoot();
-        t.setTrie(f.loadWordsFile("/home/caio/dictionary"));
         Window w = new Window();
         
         w.subscribe(t);           //The root listen to the window
