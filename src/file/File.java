613bba8e33966e34b0befb54e03ca82eee74d935
caio <caio@caio>
2019-11-24 14:42:31 -0300

    Refatoração do Main e do File 	-> Main: 		* Correção de segurança para evitar crash do programa(Adicionado 			bloco try-catch). 	-> File: 		* Correção do nome de variáveis

--- src/file/File.java
+++ src/file/File.java
@@ -6,15 +6,16 @@ import structure.*;
 
 //Class that manipulates a file
 public class File {
+
     /* Method that check if the word is a valid one
      * Input:        Word to be checked
      * Return:       True if it's a valid word or false otherwise
      * Precondition: None
-    */
+     */
     public Boolean WordCheck(String line) {
-    	return line.matches("[A-Za-z]+");
+        return line.matches("[A-Za-z]+");
     }
-    
+
     /*
      * Method that removes blank spaces in string
      * Input:        None
@@ -22,52 +23,53 @@ public class File {
      * Precondition: None
      */
     public String removeWhiteSpace(String str) {
-    	int i;
-    	String[] aux;
-    	String nova = "";
-    	aux = str.split(" ");
-    	for(i=0;i<aux.length;i++)
-    		nova+=aux[i];    		
-    	return nova;
+        int i;
+        String[] aux;
+        String newString = "";
+        aux = str.split(" ");
+        for (i = 0; i < aux.length; i++) {
+            newString += aux[i];
+        }
+        return newString;
     }
-    
+
     /* Method that load the file with the strings to be added to the tree
      * Input:        File path to be loaded
      * Return:       Trie class filled with the read words
      * Precondition: None
      */
-    public Trie loadWordsFile(String filePath) throws Exception{
+    public Trie loadWordsFile(String filePath) throws Exception {
         Trie trie = new Trie();
-        FileReader Read = new FileReader(filePath);
-        BufferedReader ReadFile = new BufferedReader(Read);
-        String Line = ReadFile.readLine();
-        while(Line != null) {
-        	Line = this.removeWhiteSpace(Line);
-        	if(this.WordCheck(Line) == true) {
-        		Line = Line.toLowerCase();
-				trie.insert(Line);
-        	}
-        	Line = ReadFile.readLine();        	
+        FileReader fileReader = new FileReader(filePath);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line = bufferedReader.readLine();
+        while (line != null) {
+            line = this.removeWhiteSpace(line);
+            if (this.WordCheck(line) == true) {
+                line = line.toLowerCase();
+                trie.insert(line);
+            }
+            line = bufferedReader.readLine();
         }
-        ReadFile.close();
+        bufferedReader.close();
         return trie;
     }
-    
+
     /* Method that load the stopwords file and remove them from the dictionary
      * Input         File path to be loaded and trie tree root
      * Return:       tree root with words removed
      * Precondition: root not null
      */
-    public TrieRoot loadStopWordsFile(String filePath, TrieRoot trie) throws Exception{
-        FileReader Read = new FileReader(filePath);
-        BufferedReader ReadFile = new BufferedReader(Read);
-        String Line = ReadFile.readLine();
-        while(Line != null) {
-        	Line = Line.toLowerCase();
-        	trie.remove(Line);
-        	Line = ReadFile.readLine();        	
+    public TrieRoot loadStopWordsFile(String filePath, TrieRoot trie) throws Exception {
+        FileReader fileReader = new FileReader(filePath);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line = bufferedReader.readLine();
+        while (line != null) {
+            line = line.toLowerCase();
+            trie.remove(line);
+            line = bufferedReader.readLine();
         }
-        ReadFile.close();
+        bufferedReader.close();
         return trie;
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,17 @@
 package main;
 
-import structure.*;
+import structure.TrieRoot;
 import window.Window;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        TrieRoot t = new TrieRoot(); //Initialize the trie
-        Window w = new Window();    //Initialize the window
-        w.subscribe(t);           //The root listen to the window
-        t.subscribe(w);           //The window listen the root and all Nodes
+    public static void main(String[] args){
+        try {
+            TrieRoot t = new TrieRoot(); //Initialize the trie
+            Window w = new Window();    //Initialize the window
+            w.subscribe(t);           //The root listen to the window
+            t.subscribe(w);           //The window listen the root and all Nodes
+        } catch (Exception ex) {
+            System.out.println("Impossível inicializar o programa");
+        }
     }
 }
