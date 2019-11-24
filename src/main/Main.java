f696caa3d898c0d4db981eeaa6da0b07a1a33c4a
caio <caio@caio>
2019-11-24 13:41:51 -0300

    Refatoração no Main 	* Adicionados comentários

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,8 +5,8 @@ import window.Window;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        TrieRoot t = new TrieRoot();
-        Window w = new Window();
+        TrieRoot t = new TrieRoot(); //Initialize the trie
+        Window w = new Window();    //Initialize the window
         w.subscribe(t);           //The root listen to the window
         t.subscribe(w);           //The window listen the root and all Nodes
     }
